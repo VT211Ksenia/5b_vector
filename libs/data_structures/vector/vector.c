75040b4206a410c0c7101c5b1bad98b9d60f5f18
Ksenia <ksyu.rudenko.2020@mail.ru>
2022-02-05 19:21:15 +0300

    memory usage of vector

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -1,4 +1,43 @@
-//
-// Created by User on 04.02.2022.
-//
+#include <stdio.h>
+#include "vector.h"
+#include <stdint.h>
+#include <malloc.h>
+#include <stdbool.h>
+
+
+vector createVector(size_t n) {
+    int *v = malloc(sizeof(int) * n);
+    if (v == NULL) {
+        fprintf(stderr, " bad alloc ");
+        exit(1);
+    }
+    return (vector) {v, 0, n};
+}
+
+void reserve(vector *v, size_t newCapacity) {
+    if (newCapacity == 0)
+        v->data = NULL;
+    else if (newCapacity < v->size)
+        v->size = newCapacity;
+    else {
+        v->data = realloc(v->data, sizeof(int) * newCapacity);
+        if (v == NULL) {
+            fprintf(stderr, " bad alloc ");
+            exit(1);
+        }
+    }
+
+}
+
+void clear(vector *v) {
+    v->size = 0;
+}
+
+void shrinkToFit(vector *v) {
+    reserve(v, v->size);
+}
+
+void deleteVector(vector *v) {
+    free(v->data);
+}
 
--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -1,6 +1,33 @@
-#include "libs/algorithms/array/array.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "C:\Users\User\CLionProjects\untitled10\libs/algorithms/array/array.h"
 
 #ifndef UNTITLED10_VECTOR_H
 #define UNTITLED10_VECTOR_H
+
+typedef struct vector{
+    int *data; // указатель на элемент вектора
+    size_t size; // размер вектора
+    size_t capacity;// вместимость вектора
+} vector;
+
+
+// возвращает структуру-дескриптор вектора из n значений
+vector createVector(size_t n);
+
+//изменяет количество памяти, выделенное под хранение элемента вектора
+void reserve(vector *v, size_t ntwCapfcity);
+
+//удаляет элементы из контейнера, не освобождая память
+void clear(vector *v);
+
+//освобождает память, выденную под неиспользуемые элементы
+void shrinkToFit(vector *v);
+
+//освобождает память, выделенную вектору
+void deleteVector(vector *v)
+
 
 #endif
