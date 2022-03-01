88f623820f3d0d2ec1d7205cb9a680a48853328e
Ksenia <ksyu.rudenko.2020@mail.ru>
2022-03-02 00:52:46 +0300

    до 4 задачи, пока это все

--- libs/data_structures/string_/string_.h
+++ libs/data_structures/string_/string_.h
@@ -12,4 +12,24 @@
 #ifndef UNTITLED10_STRING__H
 #define UNTITLED10_STRING__H
 
+size_t strLen_(const char *begin);
+
+char* find(char *begin, char *end, int ch);
+
+char* findNonSpace(char *begin);
+
+char* findSpace(char *begin);
+
+char* findNonSpaceReverse(char *rbegin, const char *rend);
+
+char* findSpaceReverse(char *rbegin, const char *rend);
+
+int strcmp ( const char *lhs , const char *rhs);
+
+char* copy(const char *beginSource, const char *endSource, char *beginDestination);
+
+char* copyIf(char *beginSource, const char *endSource, char *beginDestination, int (*f)(int));
+
+char* copyIfReverse(char *rbeginSource, const char *rendSource, char *beginDestination, int (*f)(int));
+
 #endif //UNTITLED10_STRING__H
